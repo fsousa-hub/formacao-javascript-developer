709283996d20d9c3a088e80e22d556e573cce007
fsousa <onics.aulas@gmail.com>
2022-09-07 23:06:38 -0300

    Arquivo aula.js da pasta 02-condicionais atualizado

---
+++ Modulo 1 - Primeiros Passos Com JavaScript/02 - condicionais/aula.js
@@ -0,0 +1,13 @@
+//Entrada
+
+const numero = 5;
+const ehNumeroPar = (numero % 2) === 0;
+
+//Testa uma condição 
+if (ehNumeroPar) {
+    //Exibe o numero par
+    console.log('O numero é par');
+} else {
+    //Exibe o numero impar
+    console.log('O numero é impar');
+}
